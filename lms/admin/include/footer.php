f268201d29f73b7575702028bf6e4049274669e2
Om yadav <157380214+Psychic-codes@users.noreply.github.com>
2025-04-27 21:03:49 +0530

    Delete lms directory

--- lms/admin/include/footer.php
+++
@@ -1,7 +0,0 @@
- <footer class="sticky-footer">
-        <div class="container my-auto">
-          <div class="copyright text-center my-auto">
-            <span>Laundry Managment System | Admin Panel</span>
-          </div>
-        </div>
-      </footer>
